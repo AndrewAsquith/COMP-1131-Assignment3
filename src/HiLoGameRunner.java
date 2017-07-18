db647376919e203246a694e5e11e409b78604969
Andrew Asquith <contact@andrewasquith.com>
2017-07-17 21:37:55 -0400

    Refactored to meet requirements of double prompt for quit

--- src/HiLoGameRunner.java
+++ src/HiLoGameRunner.java
@@ -23,50 +23,76 @@ public class HiLoGameRunner {
 		// instance of the game
 		HiLoGame game = new HiLoGame();
 
+		// the user's response the play again question
+		String playAgainResponse;
+
 		// the result of the user's guess
 		int guessResult = 0;
 
-		// indicator if they want to keep playing
-		boolean keepPlaying = true;
-
+		// outer loop to play multiple times
 		do {
 			// reset the game state
 			game.newGame();
 
-			System.out.print("Guess the number (enter \"restart\" to start over, or anything else to quit): ");
+			// container for the user's input
+			int userInput = -1;
 
-			// while they keep entering numbers and have not gotten the right one
-			while ((!game.getGuessedCorrectly()) && (inputReader.hasNextInt())) {
+			// while they keep entering numbers and have not gotten the right one or quit
+			while ((!game.getGuessedCorrectly()) && (userInput != 0)) {
 
-				// get the result from the game
-				guessResult = game.makeGuess(inputReader.nextInt());
+				System.out.print("Guess the number (enter 0 quit): ");
 
-				// the game class returns 1 if the guess is high
-				if (guessResult == 1) {
-					System.out.print("High" + System.lineSeparator() + "Guess again: ");
-					// the game class returns -1 if the guess is low
-				} else if (guessResult == -1) {
-					System.out.print("Low" + System.lineSeparator() + "Guess again: ");
-					// the game class returns 0 if the guess is correct
-				} else {
-					System.out.println("Correct!");
-					if (game.getGuessCount() == 1) {
-						System.out.println("You got it your first guess!");
+				// if the user entered a number
+				if (inputReader.hasNextInt()) {
+
+					// get the input
+					userInput = inputReader.nextInt();
+
+					// if the user didn't say they want to quit
+					if (userInput != 0) {
+
+						// get the result from the game
+						guessResult = game.makeGuess(userInput);
+
+						// the game class returns 1 if the guess is high
+						if (guessResult == 1) {
+							System.out.print("High" + System.lineSeparator() + "Guess again: ");
+							// the game class returns -1 if the guess is low
+						} else if (guessResult == -1) {
+							System.out.print("Low" + System.lineSeparator() + "Guess again: ");
+							// the game class returns 0 if the guess is correct
+						} else {
+							//user guessed correctly
+							System.out.println("Correct!");
+							
+							//pretty print the number of guesses
+							if (game.getGuessCount() == 1) {
+								System.out.println("You got it your first guess!");
+							} else {
+								System.out.println("You took " + game.getGuessCount() + " guesses.");
+							}
+						}
 					} else {
-						System.out.println("You took " + game.getGuessCount() + " guesses.");
+						// otherwise, say sorry to see them go
+						System.out.println("Sorry you don't want to continue this game. ");
 					}
+
+				} else {
+					// user didn't enter a number, discard and try again
+					inputReader.next();
+					System.out.println("Please enter a number!");
 				}
 			}
 
-			// if the user didn't quit, ask if the user wants to play again
-			if (game.getGuessedCorrectly()) {
-				System.out.print("Enter \"restart\" to play again, anything else to quit: ");
-			}
-			
-			//keep playing if user entered restart
-			keepPlaying = inputReader.next().equalsIgnoreCase("restart");
+			do {
+				// prompt to play again and force a y/n response
+				System.out.print("Play again? (Y/N): ");
+				playAgainResponse = inputReader.next();
 
-		} while (keepPlaying);
+			} while (!playAgainResponse.equalsIgnoreCase("y") && (!playAgainResponse.equalsIgnoreCase("n")));
+
+		// play again if user said yes
+		} while (playAgainResponse.equalsIgnoreCase("y"));
 
 		// close the input reader and thank the user for playing
 		inputReader.close();
